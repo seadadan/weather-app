e5c830f423583aa11f4849d57a355e4411498fbb
seadadan <siciidcawad@gmail.com>
2021-07-26 01:56:10 +0300

    finish it but i cant find temp in c

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // write code below
+document.querySelector(".btn").addEventListener("click",function() {
+	var input=document.querySelector("#search").value;
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=08102d50157d7a9fa60d1a78e125f463`)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+	document.querySelector("#location").innerHTML=data.name
+	document.querySelector(".desc").innerHTML=data.weather[0].description
+	document.querySelector(".c").innerHTML=data.main.temp
+	})
+	      
+      });
